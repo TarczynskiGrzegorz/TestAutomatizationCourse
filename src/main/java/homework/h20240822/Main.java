3fe05c619bbd572d7430ccac56a200dc21d70d2a
TarczynskiGrzegorz <tarczynski.tg@gmail.com>
2024-08-22 16:49:14 +0200

    psvm - check static methods

--- src/main/java/homework/h20240822/Main.java
+++ src/main/java/homework/h20240822/Main.java
@@ -12,6 +12,13 @@ public class Main {
 
         calls.stream().forEach(Callable::call);
 
+        //check static methods and fields;
+        Phone p4 = new MobilePhone();
+        Phone p5 = new MobilePhone();
+        Phone p6 = new MobilePhone();
+
+        System.out.println(p6.getId());
+        System.out.println(Phone.getPhonesInstancesAmount());
 
     }
 }
