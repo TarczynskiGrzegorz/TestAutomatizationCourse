21079cf6f4168f844aad141abeb25c93a7486b8f
TarczynskiGrzegorz <tarczynski.tg@gmail.com>
2024-09-01 20:38:11 +0200

    maven - added dependecy - log4j

--- src/main/java/homework/h20240209/MavenDependency.java
+++ src/main/java/homework/h20240209/MavenDependency.java
@@ -1,16 +1,25 @@
 package homework.h20240209;
 
 import com.google.common.collect.Lists;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class MavenDependency {
+    private static final Logger log = LogManager.getLogger(MavenDependency.class);
+
     public static void main(String[] args) {
         // Google guava library
-        List<String> names = Lists.newArrayList("Alex","Ivan","Peter","John");
+        List<String> names = Lists.newArrayList("Alex", "Ivan", "Peter", "John");
         List<String> reversed = Lists.reverse(names);
         System.out.println(reversed);
 
-
+        //log4j
+        log.debug("Debug");
+        log.info("Info");
+        log.warn("Warn");
+        log.error("Error");
+        log.fatal("Fatal");
     }
 }
